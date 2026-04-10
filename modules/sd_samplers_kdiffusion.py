365cf627530639941772bee292390921cf29491b
Haoming <hmstudy02@gmail.com>
2026-04-10 14:13:45 +0800

    typo

--- modules/sd_samplers_kdiffusion.py
+++ modules/sd_samplers_kdiffusion.py
@@ -21,7 +21,7 @@ samplers_k_diffusion = [
     ("Euler a", "sample_euler_ancestral", ["k_euler_a", "k_euler_ancestral"], {"uses_ensd": True}),
     ("Euler", "sample_euler", ["k_euler"], {}),
     ("Euler Negative", "sample_euler_negative_RF", ["k_euler_negative"], {}),
-    ("ER SDE", "sample_er_sde", ["er_side"], {}),
+    ("ER SDE", "sample_er_sde", ["er_sde"], {}),
     ("LCM", "sample_lcm", ["k_lcm"], {}),
     ("LMS", "sample_lms", ["k_lms"], {}),
     ("Heun", "sample_heun", ["k_heun"], {"second_order": True}),
