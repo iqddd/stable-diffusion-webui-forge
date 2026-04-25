800b457f134771ad602ae2484dbd1fcb05e654f8
Haoming <hmstudy02@gmail.com>
2026-04-26 01:00:01 +0800

    simplify
    (cherry picked from commit 8203a543bd62b47b626562026c50b7bc629a4842)

--- modules_forge/packages/huggingface_guess/detection.py
+++ modules_forge/packages/huggingface_guess/detection.py
@@ -212,39 +212,21 @@ def detect_unet_config(state_dict: dict, key_prefix: str):
         dit_config = {}
         assert "{}llm_adapter.blocks.0.cross_attn.q_proj.weight".format(key_prefix) in state_dict_keys
         dit_config["image_model"] = "anima"
-        dit_config["max_img_h"] = 240
-        dit_config["max_img_w"] = 240
-        dit_config["max_frames"] = 128
-        concat_padding_mask = True
-        dit_config["in_channels"] = int(state_dict["{}x_embedder.proj.1.weight".format(key_prefix)].shape[1] / 4) - int(concat_padding_mask)
+        dit_config["in_channels"] = int(state_dict["{}x_embedder.proj.1.weight".format(key_prefix)].shape[1] / 4) - 1
+        assert dit_config["in_channels"] == 16
         dit_config["out_channels"] = 16
         dit_config["patch_spatial"] = 2
         dit_config["patch_temporal"] = 1
         dit_config["model_channels"] = int(state_dict["{}x_embedder.proj.1.weight".format(key_prefix)].shape[0])
-        dit_config["concat_padding_mask"] = concat_padding_mask
-        dit_config["crossattn_emb_channels"] = 1024
-        dit_config["pos_emb_cls"] = "rope3d"
-        dit_config["pos_emb_learnable"] = True
-        dit_config["pos_emb_interpolation"] = "crop"
-        dit_config["min_fps"] = 1
-        dit_config["max_fps"] = 30
-
-        dit_config["use_adaln_lora"] = True
-        dit_config["adaln_lora_dim"] = 256
         assert dit_config["model_channels"] == 2048
+        dit_config["concat_padding_mask"] = True
+        dit_config["crossattn_emb_channels"] = 1024
+        dit_config["adaln_lora_dim"] = 256
         dit_config["num_blocks"] = 28
         dit_config["num_heads"] = 16
-
-        assert dit_config["in_channels"] == 16
-        dit_config["extra_per_block_abs_pos_emb"] = False
         dit_config["rope_h_extrapolation_ratio"] = 4.0
         dit_config["rope_w_extrapolation_ratio"] = 4.0
         dit_config["rope_t_extrapolation_ratio"] = 1.0
-
-        dit_config["extra_h_extrapolation_ratio"] = 1.0
-        dit_config["extra_w_extrapolation_ratio"] = 1.0
-        dit_config["extra_t_extrapolation_ratio"] = 1.0
-        dit_config["rope_enable_fps_modulation"] = False
 
         return dit_config
 
